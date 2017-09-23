6ccc59f335cadf2cf70ffae18ec95bdd5069e273
lwg82 <liweiguang82@163.com>
2017-09-23 11:57:29 +0800

    lua print dump

--- src/lua-workspace/chap02.cpp
+++ src/lua-workspace/chap02.cpp
@@ -11,15 +11,127 @@
 
 */
 
+void lua_table_dump(lua_State *L, int index)
+{
+	int top = lua_gettop(L);
 
+	// 把栈上给定索引处的元素作一个副本压栈
+	lua_pushvalue(L, index);
+
+	lua_pushnil(L);
+
+	fprintf(stdout, "{\n");
+
+	while(lua_next(L, -2) != 0) // key + value
+	{
+		fprintf(stdout, "\t");
+
+		// print key
+		int key_type = lua_type(L, -2);
+		switch(key_type)
+		{
+			case LUA_TSTRING:
+		             fprintf(stdout, "%s", lua_tostring(L, -2));
+			break;
+
+			case LUA_TBOOLEAN:
+			     fprintf(stdout, "%s", lua_toboolean(L, -2) ? "true" : "false");
+			break;
+
+			case LUA_TNUMBER:
+			     fprintf(stdout, "%g", lua_tonumber(L, -2));
+			break;
+
+			default:
+			    fprintf(stdout, "%s:%p", lua_typename(L, -2), lua_topointer(L, -2));
+			break;
+		}
+		
+
+		// print value
+		fprintf(stdout, "\t\t=\t");
+
+		int value_type = lua_type(L, -1);
+
+		switch(value_type)
+		{
+			case LUA_TSTRING:
+		         fprintf(stdout, "%s", lua_tostring(L, -1));
+			break;
+
+			case LUA_TBOOLEAN:
+			     fprintf(stdout, "%s", lua_toboolean(L, -1) ? "true" : "false");
+			break;
+
+			case LUA_TNUMBER:
+			     fprintf(stdout, "%g", lua_tonumber(L, -1));
+			break;
+
+			case LUA_TFUNCTION:
+			     fprintf(stdout, "%s:%p", lua_typename(L, -1), lua_topointer(L, -1));
+			break;
+
+			case LUA_TUSERDATA:
+			     fprintf(stdout, "%s:%s", lua_typename(L, -1), lua_tostring(L, -1));
+			break;
+
+			default:
+			    fprintf(stdout, "%s:%p", lua_typename(L, -1), lua_topointer(L, -1));
+			break;
+		}
+
+		//
+
+		fprintf(stdout, "\n");
+		
+		lua_pop(L, 1);
+	}
+
+	fprintf(stdout, "}\n");
+
+	lua_settop(L, top);
+}
 
 // 打印栈
 void lua_stack_dump(lua_State *L)
 {
 	int i = 0;
 	int top = lua_gettop(L);
+
 	printf("-------------------- lua start %d --------------------\n", top);
-	
+
+	for(int i=1; i<=top; ++i)
+	{
+		int t = lua_type(L, i); // 获取类型
+
+		printf("type: %s, value:", lua_typename(L, t));
+
+
+		switch(t)
+		{
+			case LUA_TSTRING:
+		             printf("%s", lua_tostring(L, i));
+			break;
+
+			case LUA_TBOOLEAN:
+		             printf(lua_toboolean(L, i) ? "true" : "false");
+			break;
+
+			case LUA_TNUMBER:
+		             printf("%g", lua_tonumber(L, i));
+			break;
+
+			case LUA_TTABLE:
+		            lua_table_dump(L, i);
+			break;
+
+			default:
+				printf("%s", lua_typename(L, t));
+			break;
+		}
+		printf("\n");
+	}	
+
 	printf("-------------------- lua end --------------------\n");
 }
 
@@ -54,7 +166,7 @@ int func_add_test1(lua_State* L)
 
 int main(int argc, char* argv[])
 {
-
+	setvbuf(stdout,NULL,_IONBF,0);
 	lua_State *L = luaL_newstate();
 		
 	luaopen_base(L);
@@ -73,7 +185,10 @@ int main(int argc, char* argv[])
 
 	// 方法3 推荐, 不会在全局库中添加
 	luaL_requiref(L, "mylibx", func_add_test1, 1);
+	lua_stack_dump(L);
 	luaL_dostring(L, "mylibx.test1(100, 300)");
+	
+	
 
 	lua_close(L);
 
